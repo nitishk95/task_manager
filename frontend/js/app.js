005ce61e7444be8e5b6c50332e10504c90b4b5f4
nitishk95 <nk792256@gmail.com>
2026-02-06 11:43:20 +0530

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/tasks";
+const API_URL = "https://task-manager-api-ag04.onrender.com";
 
 const taskForm = document.getElementById("taskForm");
 const taskList = document.getElementById("taskList");
